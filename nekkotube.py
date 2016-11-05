b4a8a7a0bdf517607744b685b31c9e3c714302cf
matthew carpenter <matt@matthews-MacBook-Pro.local>
2016-11-05 11:40:29 -0700

    swagger; updated requirements.txt

--- nekkotube.py
+++ nekkotube.py
@@ -1,21 +1,43 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask_restful import Resource, Api
+from flask_restful_swagger import swagger
 
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+api = swagger.docs(Api(app), apiVersion='0.1')
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
 
-app.config.from_envvar('NEKKOTUBE_SETTINGS', silent=True)
+class Search(Resource):
+    "Search Indexed Videos"
+    @swagger.operation(
+        notes='Searches indexed Youtube videos',
+        nickname='search',
+        parameters=[
+            {
+                'name': 'query',
+                'description': 'search query',
+                'type': 'string',
+                'paramType': 'query'
+
+            }
+        ],
+        responseMessages=[
+            {
+                'code': 200,
+                'message': 'success'
+            }
+        ]
+    )
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(Search, '/search')
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
