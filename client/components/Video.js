d849e540f7652e822672db753e379969bb00c41a
matthew carpenter <matt@matthews-MacBook-Pro.local>
2016-11-06 11:16:47 -0800

    add bundle.js to gitignore

--- client/components/Video.js
+++ client/components/Video.js
@@ -2,26 +2,33 @@ import React from 'react'
 import YouTube from 'react-youtube'
 
 const opts = {
-      height: '390',
-      width: '640',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
+    height: '390',
+    width: '640',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
         autoplay: 1
-      }
-    };
+    }
+ };
 
-function onReady(e) {
-  var p = e.target
-  setTimeout(function () {
-    console.log(p.getMediaReferenceTime());
-  },4000);
-}
+class Video extends React.Component {
+  componentDidMount() {
 
-export default function Foo() {
-  return (
-    <YouTube
+  }
+
+  onReady(event) {
+    setTimeout(function () {
+        console.log(event.target.getMediaReferenceTime());
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <YouTube
         videoId="2g811Eo7K8U"
         opts={opts}
-        onReady={onReady}
-    />
-  );
-};
+        onReady={this.onReady}
+      />
+    );
+  }
+}
+
+export default Video
