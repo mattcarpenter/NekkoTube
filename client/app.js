ad86b3a4039b08c9a48c6915ca91df4eacc7f10a
matthew carpenter <matt@matthews-MacBook-Pro.local>
2016-11-06 09:17:47 -0800

    initial react application

---
+++ client/app.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
+import { App, Home, Foo, Bar } from './components'
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="foo" component={Foo}/>
+          <Route path="bar" component={Bar}/>
+        </Route>
+      </Router>
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById('mount')
+)
---
+++ client/components/index.js
@@ -0,0 +1,5 @@
+
+export App from './App'
+export Home from './Home'
+export Foo from './Foo'
+export Bar from './Bar'
